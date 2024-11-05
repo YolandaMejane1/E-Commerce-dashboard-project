7cb319704083c48ef87203c185a159338244e810
Yolanda Mejane <mysivuyile@gmail.com>
2024-11-05 05:23:26 +0200

    "finished dashboard layout"

--- src/components/MenuSidebar.jsx
+++ src/components/MenuSidebar.jsx
@@ -1,38 +1,27 @@
 import React from 'react';
-import App from '../App';
 import { MenuData } from './MenuData';
-import {Logo} from '../assets/icons'
-
-
+import { Logo } from '../assets/icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MenuSidebar() {
   return (
-    
-     
-
-     <div className="Sidebar">
-      <span className="header"><img src={Logo} alt="" /></span>
-      <h2>Hiphonic</h2>
-
+    <div className="Sidebar d-none d-md-block"> 
+      <div className="header">
+        <h2><img src={Logo} alt="Logo" /> Hiphonic</h2>
+      </div>
       <ul className="List">
-        {MenuData.map((val,key)=> {
-          return (
+        {MenuData.map((val, key) => (
           <li 
-          key ={key} 
-          className="row"
-          onClick={()=>{window.location.pathname = val.link}}> 
-            {" "}
-             <div id="icons">{val.icon}</div>
-             <div id="name">
-              {val.title}
-             </div>
-             </li>);
-        })}
-        </ul>
-     </div>
-
+            key={key} 
+            className="row" 
+            onClick={() => { window.location.pathname = val.link }}>
+            <div id="icons">{val.icon}</div>
+            <div id="name">{val.title}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default MenuSidebar;
-
